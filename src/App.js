73ab2edd1685779856cc705a5ed28641d4ba76eb
sadikhossain69 <sadikhossain433@gmail.com>
2022-05-12 02:02:02 +0600

    Simple hero page done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
+import HeroPart from './components/HeroPart';
+import Navbar from './components/Navbar';
+import SocialIcons from './components/SocialIcons';
+import bgBanner from './Images/bg-banner.jpg'
 
 function App() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+    <div style={{backgroundImage: `url(${bgBanner})`,backgroundSize: 'cover'}}className='h-screen text-white'>
+      <Navbar/>
+      <HeroPart/>
+      <SocialIcons/>
     </div>
   );
 }
